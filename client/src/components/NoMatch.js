445588ff2481f3b98948f3e54073c0e5c78af844
joshua-murphy <josh.murphy11@gmail.com>
2017-12-16 10:19:41 -0700

    404 page

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 class NoMatch extends Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>
-        Page Not Found
-        <Link to='/'> Home</Link>
+      <Header textAlign='center'>
+        <br/><h1>Whoops!</h1>
+        <h3>The page you are looking for doesn't appear to exist in the site directory.</h3>
+        <h3>We're not sure where you wanted to go, but you're free to keep trying to get there if you like.</h3>
+        <h4>We won't stop you.</h4>
       </Header>
     );
   }
