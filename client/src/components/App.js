3e90630344131846782d8f3ca3f13e3d006c61c9
Zach Byers <byerszach@gmail.com>
2017-12-16 12:29:55 -0700

    HOC progress

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -21,7 +21,7 @@ class App extends Component {
         <Flash />
         <FetchUser>
           <Switch>
-            <Route exact path='/' component={Home} />
+            <Route path='/users' component={FetchAllUsers} />
             <ProtectedRoute exact path='/mypage' component={MyPage} />
             <ProtectedRoute exact path='/mycomments' component={MyComments} />
             <AuthRoute exact path='/login' component={Login} />
--- client/src/components/FetchAllUsers.js
+++ client/src/components/FetchAllUsers.js
@@ -2,13 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
 import Home from './Home'
+import UserPage from './UserPage'
 import { Loader, Segment, Dimmer } from 'semantic-ui-react'
+import { getUsers } from '../actions/users'
 
 class FetchAllUsers extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(getUsers())
+  }
+
   render() {
     return(
       <Segment basic>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/users' component={Home} />
+        <Route exact path='/users/:id' component={UserPage} />
       </Segment>
     )
   }
