6ad853438e61b19162165c01ffe2dfbbbfdb388c
Zach Byers <byerszach@gmail.com>
2017-12-16 15:18:49 -0700

    bug fix

--- client/src/components/FetchAllPosts.js
+++ client/src/components/FetchAllPosts.js
@@ -59,7 +59,7 @@ class FetchAllUsers extends React.Component {
 
   render() {
     const { user, pageId } = this.props
-    let email = user ? user.email : ''
+    let email = pageId ? pageId.email : ''
     if( this.state.renderNew )
       return(
         <Segment basic>
